41cc141f8eda3c29259d11253ad40dd9b6425f18
DE41H <sreyash.s@outlook.com>
2026-06-09 20:49:48 +0530

    refactored

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-
 import 'package:buritto/app.dart';
+import 'package:buritto/hive/hive_database.dart';
 import 'package:buritto/logic/filter.dart';
 import 'package:buritto/logic/network.dart';
-import 'package:buritto/hive/hive_database.dart';
 import 'package:buritto/logic/security.dart';
 import 'package:buritto/providers/home_provider.dart';
 import 'package:buritto/providers/settings_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:worker_manager/worker_manager.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
   await (
     HiveDatabase().init(),
     workerManager.init(isolatesCount: 2),
+    BiometricAuth().init(),
   ).wait;
   await (
     BiometricAuth().lock(),
