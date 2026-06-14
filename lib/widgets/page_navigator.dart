55992ac422d4237820e789e1d8156f50d2a3f7ac
DE41H <sreyash.s@outlook.com>
2026-06-15 01:57:52 +0530

    added infinite scroll

--- lib/widgets/page_navigator.dart
+++ lib/widgets/page_navigator.dart
@@ -6,16 +6,18 @@ class PageNavigator extends StatelessWidget {
   const PageNavigator({super.key});
 
   static final PageController _controller = PageController(initialPage: 1);
+  static const int _limit = 100000000;
+  static const _pages = [SettingsPage(), HomePage()];
+
+  Widget _itemBuilder(BuildContext context, final int index) => _pages[index % _pages.length];
 
   @override
   Widget build(BuildContext context) {
-    return PageView(
+    return PageView.builder(
       controller: _controller,
+      itemCount: _limit,
       physics: const BouncingScrollPhysics(),
-      children: const [
-        SettingsPage(),
-        HomePage(),
-      ],
+      itemBuilder: _itemBuilder,
     );
   }
 }
