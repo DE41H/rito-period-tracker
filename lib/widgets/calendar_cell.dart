3fa2a55ad3c9704e235571810fe51eeebdd10451
DE41H <sreyash.s@outlook.com>
2026-06-18 23:58:07 +0530

    created structure for widget display builder

--- lib/widgets/calendar_cell.dart
+++ lib/widgets/calendar_cell.dart
@@ -7,26 +7,47 @@ class CalendarCell extends StatelessWidget {
 
   final QuantumLog? q;
 
+  void _onTap(BuildContext context, QuantumLog? q) {
+    showModalBottomSheet<void>(
+      showDragHandle: true,
+      context: context,
+      constraints: const BoxConstraints(
+        maxWidth: double.infinity,
+      ),
+      builder: (BuildContext context) {
+        return SizedBox(
+          width: double.infinity,
+          child: Text(
+            q.hashCode.toString(),
+          ),
+        );
+      }
+    );
+  }  
+  
   @override
   Widget build(BuildContext context) {
     final double progress = q == null ? 0.0 : (q!.cycleDay / KalmanFilter().cycleLength).clamp(0.0, 1.0);
 
-    return Card(
-      elevation: 0,
-      child: Stack(
-        children: [
-          Center(
-            child: Text(
-              '${(progress == 0) ? '' : q!.date.day }'
+    return InkWell(
+      onTap: () => _onTap(context, q),
+      child: Card(
+        elevation: 0,
+        child: Stack(
+          children: [
+            Center(
+              child: Text(
+                '${(progress == 0) ? '' : q!.date.day }'
+              ),
             ),
-          ),
-          if (progress != 0) Center(
-            child: CircularProgressIndicator(
-              value: progress,
-              color: Colors.black,
-            ),
-          )
-        ],
+            if (progress != 0) Center(
+              child: CircularProgressIndicator(
+                value: progress,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
