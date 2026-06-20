45700c0099ebbd7ebf6d2189a9716cd5a4c1e82d
DE41H <sreyash.s@outlook.com>
2026-06-20 23:56:33 +0530

    added popup sheet

--- lib/widgets/calendar_cell.dart
+++ lib/widgets/calendar_cell.dart
@@ -1,36 +1,19 @@
 import 'package:buritto/logic/filter.dart';
 import 'package:buritto/models/quantum.dart';
+import 'package:buritto/sheets/quantum_log_modal.dart';
 import 'package:flutter/material.dart';
 
 class CalendarCell extends StatelessWidget {
   const CalendarCell({super.key, this.q});
 
   final QuantumLog? q;
-
-  void _onTap(BuildContext context, QuantumLog? q) {
-    showModalBottomSheet<void>(
-      showDragHandle: true,
-      context: context,
-      constraints: const BoxConstraints(
-        maxWidth: double.infinity,
-      ),
-      builder: (BuildContext context) {
-        return SizedBox(
-          width: double.infinity,
-          child: Text(
-            q.hashCode.toString(),
-          ),
-        );
-      }
-    );
-  }  
   
   @override
   Widget build(BuildContext context) {
     final double progress = q == null ? 0.0 : (q!.cycleDay / KalmanFilter().cycleLength).clamp(0.0, 1.0);
 
     return InkWell(
-      onTap: () => _onTap(context, q),
+      onTap: () => (q == null) ? null : QuantumLogModal().show(context, q!),
       child: Card(
         elevation: 0,
         child: Stack(
