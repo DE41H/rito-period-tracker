39bae5758b30344c306a8032967a3a215d254ac4
DE41H <sreyash.s@outlook.com>
2026-06-21 01:38:59 +0530

    simplified provider

--- lib/providers/home_provider.dart
+++ lib/providers/home_provider.dart
@@ -2,22 +2,15 @@ import 'package:buritto/models/message.dart';
 import 'package:flutter/material.dart';
 
 class HomeProvider extends ChangeNotifier {
-  final TextEditingController inputController = TextEditingController();
-
   double _progress = 0.4;
   double get progress => _progress;
   set progress(final double value) {
     _progress = value.clamp(0.0, 1.0);
   }
 
-  void sendMessage([final bool isInput = true]) {
-    MessageRepo().send(inputController.text.trim(), isInput);
-    inputController.clear();
-  }
+  TextEditingController get inputController => MessageRepo().controller;
 
-  @override
-  void dispose() {
-    inputController.dispose();
-    super.dispose();
+  void sendMessage() {
+    MessageRepo().send(inputController.text.trim());
   }
 }
